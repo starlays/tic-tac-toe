e1bc50fcb1da9a1179e7f42947a22d8f3dc40a02
Lazar Florentin <florentin.lazar@gmail.com>
2014-02-25 23:32:03 +0200

    Add HTTPRequest class test

---
+++ tests/HTTPRequestTest.php
@@ -0,0 +1,22 @@
+<?php
+class HTTPRequest extends PHPUnit_Framework_TestCase
+{
+    public function testProcessURI()
+    {
+        $httpRequest = new \tictactoe\HTTPRequest();
+        $result = $httpRequest->processURI('http://example.com/foo?bar=baz');
+
+        $this->assertNotEmpty($result);
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testException()
+    {
+        $this->setExpectedException('\RuntimeException');
+
+        $httpRequest = new \tictactoe\HTTPRequest();
+        $result = $httpRequest->processURI('!'); // <-- Invalid URI
+    }
+}
