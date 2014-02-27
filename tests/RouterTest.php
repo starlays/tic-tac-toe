88e3d60e740a5806c509099ec6bb4db1bed7e56f
Lazar Florentin <florentin.lazar@gmail.com>
2014-02-27 21:01:20 +0200

    Updated RouterTest file

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -1,10 +1,17 @@
 <?php
-class RouterTest extends PHPUnit_Framework_TestCase
+namespace RouterTest;
+require_once "../src/Router.php";
+require_once "../src/HTTPRequest.php";
+
+class RouterTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGetRoute()
+    public function MathcRoute()
     {
-        $router = new \Tic-Tac-Toe\Router;
-        $route  = $router->route('GET route');
+        $router = new \tictactoe\Router;
+        $httpRequest = new \tictactoe\HTTPRequest();
+        
+        $request = $httpRequest->processURI('http://example.com/foo?bar=baz');
+        $route  = $router->MatchRoute($request, $routes);
         
         $this->assertNotEmpty($route);
     }
